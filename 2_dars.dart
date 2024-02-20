ead1982b4584bc2fd4fc16c48d2c9a1f636ff150
Ramazon <ergashevrama2010@gmail.com>
2024-02-20 13:45:04 +0500

    Initial commit

---
+++ 2_dars.dart
@@ -0,0 +1,21 @@
+main(List<String> args) {
+  // print("Ramazon");
+  // // O'zgaruvchilar va ma'lumot turlari
+  // // double qoldiq son
+  // String a = "Ramazon";
+  // print(a);
+  // int b = 8;
+  // double c = b.toDouble();
+  // print(c);
+  // double e = 8.9;
+  // int f = e.toInt();
+  // print(f);
+  // String ism = "Ramazon ";
+  // String familiya = "Ergashev";
+  // String kurs = " Dartning o\'zi";
+  // print(ism + familiya + kurs);
+  // print("Malumotlar ism : ${ism.length}, \n familiya : $familiya");
+  // int son = 50;
+  // double qoldiq = 52.26;
+  // print("Birinchi Son:" + son.toString());
+}
---
+++ index.dart
@@ -0,0 +1,6 @@
+main(List<String> args) {
+  print("Salom");
+  print("7+4");
+  print(7 + 4);
+  print("O\'zimizning"); // "" ichida yana bitta qo'shtirnoq endi kela oladi
+}
